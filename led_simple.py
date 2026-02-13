2f6e1b555008deae40ef1d492ef7c7505d2f53c1
Mikael Rouleau <202162803@cegepsherbrooke.qc.ca>
2026-02-13 15:24:53 -0500

    feat(led): ajout contrôle simple des LEDs

--- led_simple.py
+++ led_simple.py
@@ -22,7 +22,28 @@ LED_VERTE = 27
 LED_JAUNE = 22
 
 # TODO : Configurer le mode BCM
-# GPIO.setmode(GPIO.???)
+
+# Configuration
+GPIO.setmode(GPIO.BCM)
+GPIO.setup([LED_ROUGE, LED_VERTE, LED_JAUNE], GPIO.OUT)
+
+# Allumer la LED rouge
+while True:
+    GPIO.output(LED_ROUGE, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_ROUGE, GPIO.LOW)
+
+#Allumer la LED verte
+    GPIO.output(LED_VERTE, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_VERTE, GPIO.LOW)
+
+#Allumer la LED jaune
+    GPIO.output(LED_JAUNE, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_JAUNE, GPIO.LOW)
+
+GPIO.cleanup()
 
 # TODO : Configurer les broches en sortie
 # GPIO.setup(..., GPIO.OUT)
